de5007bd1ddcb72692909c7a7207adff60423289
HKCS04 <103810087+HKCS04@users.noreply.github.com>
2025-05-03 21:46:46 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,15 +6,15 @@ class Config:
     API_ID = "22136772"
     API_HASH = "7541e5b6d298eb1f60dac89aae92868c"
     BOT_TOKEN = "7587525827:AAEDwwPfawc0WyTHMMxLknP45Fic3qKQrh8"
-    SESSION_NAME = "ScreenshotGenRobot
+    SESSION_NAME = "ScreenshotGenRobot"
     LOG_CHANNEL = "-1002638440257"
     DATABASE_URL = "mongodb+srv://storymachineofficial:storymachineofficial@cluster0.snchtpl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     AUTH_USERS = [8083702486]
     MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 5))
     MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 10000))
-    TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', True))
+    TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', False))
     SLOW_SPEED_DELAY = int(os.environ.get('SLOW_SPEED_DELAY', 5))
-    HOST = os.environ.get('HOST', '')
+    HOST = os.environ.get('HOST', 'http://localhost:8080')
     
     SCRST_OP_FLDR = Path('screenshots/')
     SMPL_OP_FLDR = Path('samples/')
