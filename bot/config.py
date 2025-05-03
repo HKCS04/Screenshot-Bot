5d7e36417101263338d249a5817e2c89f3773e4b
HKCS04 <103810087+HKCS04@users.noreply.github.com>
2025-05-03 21:39:33 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,17 +3,17 @@ from pathlib import Path
 
 class Config:
     
-    API_ID = int(os.environ.get('API_ID'))
-    API_HASH = os.environ.get('API_HASH')
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    SESSION_NAME = os.environ.get('SESSION_NAME')
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL'))
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '').split(' ')]
-    MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 2))
-    MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 600))
-    TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', False))
-    SLOW_SPEED_DELAY = int(os.environ.get('SLOW_SPEED_DELAY', 15))
+    API_ID = "22136772"
+    API_HASH = "7541e5b6d298eb1f60dac89aae92868c"
+    BOT_TOKEN = "7587525827:AAEDwwPfawc0WyTHMMxLknP45Fic3qKQrh8"
+    SESSION_NAME = "ScreenshotGenRobot
+    LOG_CHANNEL = "-1002638440257"
+    DATABASE_URL = "mongodb+srv://storymachineofficial:storymachineofficial@cluster0.snchtpl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    AUTH_USERS = [8083702486]
+    MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 5))
+    MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 10000))
+    TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', True))
+    SLOW_SPEED_DELAY = int(os.environ.get('SLOW_SPEED_DELAY', 5))
     HOST = os.environ.get('HOST', '')
     
     SCRST_OP_FLDR = Path('screenshots/')
